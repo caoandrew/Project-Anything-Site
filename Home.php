c7b70639d84dceda1feeb599e7bb051b572fdc4c
PieCheeseGuy <stormtrooper5510@gmail.com>
2018-10-07 09:45:13 -0500

    Change

--- Home.php
+++ Home.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Project Anything</title>
-  </head>
+	<head>
+		<title>Project Anything</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body>
+		<ul>
+			<li><img src="communaseed.png" alt="Communaseed Logo" style="width:150px;height:40px;margin-top:0;margin-bottom:0;margin-left:15px;margin-right:10px;"></li>
+ 			<li><a class="active" href="#home">Home</a></li>
+ 			<li><a href="#Plant">Start a Project</a></li>
+  			<li><a href="#Grow">Fund a Project</a></li>
+  			<li style="float:right"><a href="#Signup">Sign Up</a></li>
+  			<li style="float:right"><a href="#Login">Log In</a></li>
+		</ul>
+	</body>
 </html>
