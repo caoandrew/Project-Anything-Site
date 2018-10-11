23f7c27c05ac9f3bf13cf415acd1d5ee7f6e2121
ctOS <stormtrooper5510@gmail.com>
2018-10-10 23:01:32 -0500

    1.0

--- Home.php
+++ Home.php
@@ -7,8 +7,8 @@
 	
 	<body>
 		<ul>
-			<li><img src="ProjectAnything.png" alt="Project Anything" style="width:150px;height:40px;margin-top:0;margin-bottom:0;margin-left:0;margin-right:0;"></li>
- 			<li><a class="active" href="#home">Project Anything</a></li>
+			<li><img src="PATrans.png" alt="Project Anything" style="width:5%;height:5.45%;margin-top:1%;margin-bottom:1%;margin-left:1%;margin-right:1%;"></li>
+ 			<li><a href="#home">Project Anything</a></li>
  			<li><a href="#Portfolio">Portfolio</a></li>
   			<li><a href="#Investing">Investing</a></li>
   			<li><a href="#Contact">Contact Us</a></li>
