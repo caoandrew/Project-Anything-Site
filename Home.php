f813aef06f7c7708e06c321d9e8abebfd5ded502
ctOS <stormtrooper5510@gmail.com>
2018-10-11 00:25:50 -0500

    1.1

--- Home.php
+++ Home.php
@@ -7,11 +7,24 @@
 	
 	<body>
 		<ul>
-			<li><img src="PATrans.png" alt="Project Anything" style="width:5%;height:5.45%;margin-top:1%;margin-bottom:1%;margin-left:1%;margin-right:1%;"></li>
+			<li><img src="PATrans.png" alt="Project Anything" href="#home"></li>
  			<li><a href="#home">Project Anything</a></li>
  			<li><a href="#Portfolio">Portfolio</a></li>
   			<li><a href="#Investing">Investing</a></li>
   			<li><a href="#Contact">Contact Us</a></li>
 		</ul>
+		
+		<br>
+		<p><center><font size="16">Project Anything</font></center></p>
+		<br>
+		<center><img src="PATrans.png" alt="Project Anything" style="width:20%;height:22.25%;"></center>
+		<br>
+		<p><center><font size="14" color="#55AEF9">CHS Club Beta Site</font></center></p>
+		<br>
+		<p><center><font size="14">Full Site Coming Soon!</font></center></p>
+		<br>
+		
+		<center><div class="lds-facebook"><div></div><div></div><div></div></div></center>
+
 	</body>
 </html>
