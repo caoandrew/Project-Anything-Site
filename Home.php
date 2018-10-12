98eb01817b568ca968614bb448c5b8f5698ece12
ctOS <stormtrooper5510@gmail.com>
2018-10-11 20:35:02 -0500

    1.3 ANIMATIONS

--- Home.php
+++ Home.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="icon" type="image/png" href="favicon (1).ico"/>
 		<title>Project Anything</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="icon" type="image/png" href="favicon (1).ico"/>
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+		
 	</head>
 	
 	<body>
+	    
+	    <div id="loader-wrapper">
+            <div id="loader"></div>
+ 
+            <div class="loader-section section-left"></div>
+            <div class="loader-section section-right"></div>
+ 
+        </div>
+	    
+	    
+	    
 		<ul>
 			<li><img src="PATrans.png" alt="Project Anything" href="#home"></li>
  			<li><a href="#home">Project Anything</a></li>
@@ -20,12 +33,25 @@
 		<br>
 		<center><img src="PATrans.png" alt="Project Anything" style="width:20%;height:22.25%;"></center>
 		<br>
-		<p><center><font size="14" color="#55AEF9">CHS Club Beta Site</font></center></p>
+		<p><center><font size="14" color="#55AEF9">Coming Soon.</font></center></p>
 		<br>
-		<p><center><font size="14">Full Site Coming Soon!</font></center></p>
+		<p><center><font size="14">Explore.</font></center></p>
 		<br>
 		
 		<center><div class="lds-facebook"><div></div><div></div><div></div></div></center>
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+	<script src="main.js"></script>
+
+		
+		<script>
+	var _gaq=[['_setAccount','UA-839919-3'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+		g.src='//www.google-analytics.com/ga.js';
+		s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
+
 
 	</body>
 </html>
