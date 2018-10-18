f6df079be91dd1e8540e0a26df3fb9e228241c1f
ctOS <stormtrooper5510@gmail.com>
2018-10-17 22:37:16 -0500

    1.1 More Animations and JS

--- main.js
+++ main.js
@@ -6,3 +6,30 @@ $(document).ready(function() {
     }, 3000);
  
 });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+     }, 500);
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 275 || document.documentElement.scrollTop > 275) {
+        document.getElementById("myBtn").style.display = "block";
+        $("#myBtn").fadeIn();
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+        $("#myBtn").fadeOut();
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+//function topFunction() {
+  //  document.body.scrollTop = 0; // For Safari
+  //  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    
+//}
