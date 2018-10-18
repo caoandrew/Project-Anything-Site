2c1c5bf69a626c87bb2bb9cd8271f9954c54682c
ctOS <stormtrooper5510@gmail.com>
2018-10-17 22:38:34 -0500

    1.5 Banners

--- Home.php
+++ Home.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link rel="icon" type="image/png" href="favicon (1).ico"/>
 		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
-		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	</head>
 	
 	<body>
@@ -20,12 +20,12 @@
 	    
 	    
 	    
-		<ul>
+		<ul id="top">
 			<li><img src="PATrans.png" alt="Project Anything" href="#home"></li>
- 			<li><a href="#home">Project Anything</a></li>
- 			<li><a href="#Portfolio">Portfolio</a></li>
-  			<li><a href="#Investing">Investing</a></li>
-  			<li><a href="#Contact">Contact Us</a></li>
+ 			<li><a href="#projectanything">Project Anything</a></li>
+ 			<li><a href="#portfolio">Portfolio</a></li>
+  			<li><a href="#investing">Investing</a></li>
+  			<li><a href="#contact">Contact Us</a></li>
 		</ul>
 		
 		<br>
@@ -39,6 +39,45 @@
 		<br>
 		
 		<center><div class="lds-facebook"><div></div><div></div><div></div></div></center>
+		
+		<a href="#top"><button onclick="topFunction()" id="myBtn" title="Go to top">Top</button></a>
+		
+		<div id="projectanything">
+            <p id="pprojectanything"><font size="7em">We are Project Anything.</font></p>
+            <p id="pprojectanything"><font size="6em">Our purpose is to spread Technological Literacy to kids and students in the local community.</font></p>
+            <p id="pprojectanything"><font size="6em">We create programs, machines, and technological devices to showcase and teach the community.</font></p>
+            <p id="pprojectanything"><font size="6em">We are glad to help the students and local community, it means a lot and gives us great experiences.</font></p>
+        </div>
+        
+        <div id="portfolio">
+            <p id="pportfolio"><font size="7em">Our Portfolio.</font></p>
+        </div>
+        
+        <div id="investing">
+            <p id="pinvesting"><font size="7em">Investing.</font></p>
+        </div>
+        
+        <div id="contact">
+            <p id="pcontact"><font size="7em">This is us.</font></p>
+        </div>
+        
+        
+        
+        <div id="headerend">
+            <center>
+            <p id="pend">Project Anything ©2018</p>
+            <p id="pendother">Contact Us</p> 
+            <p id="pendother">Investor Document</p>
+            <p id="pendother">Advisor Contact</p>
+            </center>
+        </div>
+		
+		
+		
+		
+		
+		
+		
 		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
